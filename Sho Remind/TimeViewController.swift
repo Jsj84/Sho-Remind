43f00580ee04fd1ef3938812fc13997a2f3c3d59
Jesse St. John <jessestjohn1@Jesses-MacBook-Pro.local>
2016-12-26 12:13:12 -0500

    second commit

--- Sho Remind/TimeViewController.swift
+++ Sho Remind/TimeViewController.swift
@@ -9,31 +9,57 @@
 import Foundation
 import UIKit
 
-class TimeViewController: UIViewController, UITextFieldDelegate {
+class TimeViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var reminderName: UILabel!
     @IBOutlet weak var reminderDiscription: UITextField!
     @IBOutlet weak var timePicker: UIDatePicker!
-    @IBOutlet weak var staticLable: UILabel!
-    @IBOutlet weak var dynamicLable: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
-    var textEntered = ""
+    var textEntered:[String] = []
+    var tableViewTemps = ["2:00pm ", "4:00pm", "7:00pm"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         reminderDiscription.delegate = self
-        staticLable.isHidden = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = UIColor.clear
+        tableView.isOpaque = true
+
  
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
-        staticLable.isHidden = false 
-        textEntered = reminderDiscription.text!
-        dynamicLable.text = textEntered
-        print(textEntered)
+        textEntered = [reminderDiscription.text!]
+        tableView.reloadData()
         return false
-        
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Saved Reminders"
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textEntered.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        for i in 0..<textEntered.count {
+        cell?.textLabel?.text = textEntered[i]
+        }
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            textEntered.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
     }
 
 }
